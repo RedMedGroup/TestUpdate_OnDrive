7e8b50770f66b33c750c0db4b6b1028e527b9e0a
roddik <comptastock.tech@gmail.com>
2024-10-18 10:16:33 +0100

    Add project files.

---
+++ TestUpdate_OnDrive/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace TestUpdate_OnDrive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.SuspendLayout();
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 12.25F, System.Drawing.FontStyle.Bold);
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Location = new System.Drawing.Point(238, 98);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(31, 19);
+            this.labelControl1.TabIndex = 0;
+            this.labelControl1.Text = "V_1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 278);
+            this.Controls.Add(this.labelControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+    }
+}
+
---
+++ TestUpdate_OnDrive/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestUpdate_OnDrive
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        void Update()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
+            WebClient webClient = new WebClient();
+
+            // رابط لملف نصي يحتوي على رقم الإصدار الأخير في GitHub Releases
+            string versionUrl = "https://raw.githubusercontent.com/username/repository/main/version.txt";
+
+            if (!webClient.DownloadString(versionUrl).Contains("1.0.0.0"))
+            {
+                if (MessageBox.Show("هل ترغب في تثبيت التحديث؟", "تحديث جديد", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    string appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                    string updaterPath = System.IO.Path.Combine(appDataFolder, @"Your tst\Sonatrach\updater.exe");
+
+                    if (File.Exists(updaterPath))
+                    {
+                        try
+                        {
+                            Process.Start(updaterPath);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("حدث خطأ أثناء تشغيل المحدث: " + ex.Message, "خطأ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("لم يتم العثور على المحدث (updater.exe) في المسار المحدد.", "خطأ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        void Update1()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
+            WebClient webClient = new WebClient();
+
+            if (!webClient.DownloadString("https://www.dropbox.com/scl/fi/zewy4jv1mnr20bc0haf33/SHUpdate.txt?rlkey=7jv5vsdyg0bcv9oxqt09fc09y&st=wqjx7flb&dl=1").Contains("1.0.0.0"))
+            {
+                if (MessageBox.Show("هل ترغب في تثبيت التحديث؟", "تحديث جديد", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    // الحصول على مسار مجلد التطبيقات المحلية باستخدام Environment.GetFolderPath
+                    string appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+                    // تكوين مسار المحدث updater.exe داخل مجلد التطبيقات المحلية
+                    string updaterPath = System.IO.Path.Combine(appDataFolder, @"Your tst\Sonatrach\updater.exe");
+
+                    //string updaterPath = @"C:\Users\Roddik\AppData\Roaming\Your TEST\TEST\updater44.exe"; // تغيير المسار حسب مكان updater.exe على جهاز المستخدم
+                    //C: \Users\Roddik\AppData\Roaming\Your tst\Sonatrach 4.0.0.0\install\Sonatrach1.cab
+                    if (File.Exists(updaterPath))
+                    {
+                        try
+                        {
+                            // فتح updater.exe
+                            Process.Start(updaterPath);
+
+                            // إغلاق التطبيق الحالي (اختياري)
+                            //this.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("حدث خطأ أثناء تشغيل المحدث: " + ex.Message, "خطأ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("لم يتم العثور على المحدث (updater.exe) في المسار المحدد.", "خطأ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+    }
+}
